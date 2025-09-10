e981c17002391f24cba5573fc69021b6ecab1a4b
ayanszone <160243995+ayanszone@users.noreply.github.com>
2025-09-10 22:19:55 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["placeholder.svg"],
+    unoptimized: true,
+  },
+  reactStrictMode: true, // keep this
+}
+
+module.exports = nextConfig
+
